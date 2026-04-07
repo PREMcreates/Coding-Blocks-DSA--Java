945fc6242746eff20eb2c76cdd030d062d59874d
fullstackNINJA05 <prempanchal0501@gmail.com>
2026-04-08 01:43:13 +0530

    DemoGithub Folder Added

--- src/DemoforGithub.java
+++
@@ -1,7 +0,0 @@
-public class DemoforGithub {
-    public static void main(String[] args) {
-        System.out.println("Hello, world.");
-        System.out.println("Prem");
-        System.out.println("Panchal");
-    }
-}
